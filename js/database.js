4f5510dff442870bfc82fd5bb288c873d8e315bc
isaswh <isa.swh@gmail.com>
2023-12-15 09:51:18 +0100

    oppdaterte fra eksempel

--- js/database.js
+++ js/database.js
@@ -1,14 +1,16 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 // Your web app's Firebase configuration
 const firebaseApp = firebase.initializeApp({
-    apiKey: "[ HER LEGGER DU INN DET DU HAR: ]",
-    authDomain: "[ HER LEGGER DU INN DET DU HAR: ]",
-    projectId:"[ HER LEGGER DU INN DET DU HAR: ]",
-    storageBucket: "[ HER LEGGER DU INN DET DU HAR: ]",
-    messagingSenderId: "[ HER LEGGER DU INN DET DU HAR: ]",
-    appId: "[ HER LEGGER DU INN DET DU HAR: ]"
+    apiKey: "AIzaSyAZ-GLjkKYDvz9n8SSmkXqO6jbpRxPdXdA",
+    authDomain: "filmdatabase-2b1f4.firebaseapp.com",
+    projectId: "filmdatabase-2b1f4",
+    storageBucket: "filmdatabase-2b1f4.appspot.com",
+    messagingSenderId: "760971591681",
+    appId: "1:760971591681:web:db914a8a424fe5e2eb0000"
 });
 
 // Initialize Firebase
@@ -17,7 +19,20 @@ const db = firebaseApp.firestore();
 
 function addMovie() {
     // Legger til info i collection som heiter "movies"
-    firebase.firestore().collection("movies").doc().set({
-        title: "test"
+    db.collection("movies").doc().set({
+        title: "test",
+        year: "2003"
     })
 }
+
+function getMovies() {
+    let messagesText = "";
+
+    db.collection("movies").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            messagesText += "<div class='movie'>" + doc.data().title + "</div>";
+        });
+        document.getElementById("movies").innerHTML = messagesText;
+    });
+}
+getMovies();
